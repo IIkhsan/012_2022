6689aa701f7ce809b87b64e79e401032eca73ab6
Ilyas <ilias.ihsanov@gmail.com>
2022-02-15 13:01:19 +0300

    Initial Commit

---
+++ Archetecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Archetecture
+//
+//  Created by ilyas.ikhsanov on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
